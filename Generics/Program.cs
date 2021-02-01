0bff60d424b5271e208c5d1acb2d71dc19a1e12a
hiwad rashad <hiwadrashad112@hotmail.com>
2021-02-01 16:12:50 +0100

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program<T>
+    {
+        public T textvalue { get; set; }
+        static void Main(string[] args)
+        {
+            genericclass<string> item = new genericclass<string>();
+
+            item.textvalue = "test this";
+
+            Console.WriteLine(item.returnvalue());
+        }
+    }
+
+    public class genericclass<T>
+    {
+    public T textvalue { get; set; }
+
+        public T returnvalue()
+        {
+            return textvalue;
+        }
+    }
+}
